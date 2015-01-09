3606ba90fc46960bc4b343918817956402ec66c9
Till Wehowski <github@webfan.de>
2015-01-09 16:42:38 +0100

    Translations
    French by @shortymc

---
+++ frdl/webfan/Install.php
@@ -0,0 +1,99 @@
+<?php
+/**
+* Hardcoded	  
+**/
+
+
+this->str = array();
+		$this->str['en'] = array( 
+		  'English',
+		  array(
+		  '__MAINTENANCE__' => '<span class="webfan-red">Our Install-Server is in maintenance mode.</span> Please try again later: <a href="http://www.webfan.de/install/">http://www.webfan.de/install/</a>',
+		  '__APPS__' => 'Applications',
+		  '__INSTALLATIONS__' => 'Installations',
+		  '__SETTINGS__' => 'Settings',
+		  '__INSTALL__' => 'install',
+		  '__INSTALL_NOW__' => 'install now',
+		  '__INSTALL_HERE__' => 'Install HERE!',
+		  '__IS_NOT_WRITABLE__' => 'is not writable',
+		  '__DOWNLOAD_FAILED__' => 'Download failed',
+		  '__CANNOT_WRITE_FILE__' => 'Cannot write file',
+		  '__CANNOT_UNZIP_FILE__' => 'Cannot unzip file',
+		  '__ACCEPT_LICENSE__' => 'Accept license',	  
+		  '__LANGUAGE__' => 'Language', 
+		  '__VERSION_UNAVAILABLE__' => 'The selected version is not available',
+		  '__ERROR__' => 'Error',
+		  '__HINT_DEL_FILE__' => 'You should delete this file when done!\nTo do so click the red button at bottom!',
+		  '__RECOMMENDED__' => 'recommended',
+		  '__THIS_FILE__' => 'This file',
+		  '__TEST_ONLY__' => 'For testing only',
+		  '__INCOMPLETE_OR_BROKEN__' => 'Incomplete or broken',
+		  '__SURE_BACKUP__' => 'sure/backup',
+		  '__CHECK_SURE__' => 'not confirmed',
+		  '__REQUIRED_PHP_VERSION_FAILED_1__' => 'Your php version does not apply to the requirements of the requested software. You need <strong>PHP Version</strong> '.htmlentities('=>').' ',
+		  
+		  ),
+		  
+		  );
+		$this->str['de'] =  array( 
+		  'Deutsch',
+		  array(
+		  '__MAINTENANCE__' => '<span class="webfan-red">Der Install-Server wird derzeit gewartet.</span> Bitte etwas sp&auml;ter nochmal probieren: <a href="http://www.webfan.de/install/">http://www.webfan.de/install/</a>',
+		  '__APPS__' => 'Anwendungen',
+		  '__INSTALLATIONS__' => 'Installationen',
+		  '__SETTINGS__' => 'Einstellungen',
+		  '__INSTALL__' => 'installieren',
+		  '__INSTALL_NOW__' => 'jetzt installieren',
+		  '__INSTALL_HERE__' => 'HIER installieren',
+		  '__IS_NOT_WRITABLE__' => 'ist nicht beschreibbar',
+		  '__DOWNLOAD_FAILED__' => 'Download fehlgeschlagen',
+		  '__CANNOT_WRITE_FILE__' => 'Kann Datei nicht schreiben',
+		  '__CANNOT_UNZIP_FILE__' => 'Kann Datei nicht entpacken',
+		  '__ACCEPT_LICENSE__' => 'Lizenz akzeptieren',	  
+		  '__LANGUAGE__' => 'Sprache', 
+		  '__VERSION_UNAVAILABLE__' => 'Die ausgew&auml;hlte Version ist nicht verf&uuml;gbar',
+		  '__ERROR__' => 'Fehler',
+		  '__HINT_DEL_FILE__' => 'Sie sollten diese Datei löschen wenn Sie fertig sind!\nKlicken Sie hierzu auf den roten Button ganz unten!',
+		  '__RECOMMENDED__' => 'empfohlen',
+		  '__THIS_FILE__' => 'Diese Datei',
+		  '__TEST_ONLY__' => 'Nur f&uuml;r Testzwecke',
+		  '__INCOMPLETE_OR_BROKEN__' => 'Unvollst&auml;ndig oder defekt',
+		  '__SURE_BACKUP__' => 'sicher/Backup',
+		  '__CHECK_SURE__' => 'nicht best&auml;tigt',
+		  '__REQUIRED_PHP_VERSION_FAILED_1__' => 'Ihre php Version gen&uuml;gt nicht den Anspr&uuml;chen der angeforderten Software. Sie ben&ouml;tigen <strong>PHP Version</strong> '.htmlentities('=>').' ',
+		  
+		  
+		  ),
+		  
+		  );
+		
+		$this->str['fr'] =  array( 
+	  'French',
+      array(
+      '__MAINTENANCE__' => '<span class="webfan-red">Notre serveur d\'installation est en cours de maintenance.</span> Merci de réessayer plus tard: <a href="http://www.webfan.de/install/">http://www.webfan.de/install/</a>',
+      '__APPS__' => 'Applications',
+      '__INSTALLATIONS__' => 'Installations',
+      '__SETTINGS__' => 'Paramètres',
+      '__INSTALL__' => 'installer',
+      '__INSTALL_NOW__' => 'installer maintenant',
+      '__INSTALL_HERE__' => 'Installer ici !',
+      '__IS_NOT_WRITABLE__' => 'n\'est pas inscriptible',
+      '__DOWNLOAD_FAILED__' => 'Téléchargement échoué',
+      '__CANNOT_WRITE_FILE__' => 'Impossible d\'écrire le fichier',
+      '__CANNOT_UNZIP_FILE__' => 'Impossible de décompresser le fichier',
+      '__ACCEPT_LICENSE__' => 'Accepter la license',  
+      '__LANGUAGE__' => 'Langage',
+      '__VERSION_UNAVAILABLE__' => 'La version choise n\'est pas disponible',
+      '__ERROR__' => 'Erreur',
+      '__HINT_DEL_FILE__' => 'Vous devriez supprimer ce fichier un fois terminé !\nPour le faire cliquer sur le bouton rouge au dessous!',
+      '__RECOMMENDED__' => 'recommendé',
+      '__THIS_FILE__' => 'Ce fichier',
+      '__TEST_ONLY__' => 'Seulement pour des test',
+      '__INCOMPLETE_OR_BROKEN__' => 'Incomplet ou cassé',
+      '__SURE_BACKUP__' => 'sauver/sauvegarder',
+      '__CHECK_SURE__' => 'non confirmé',
+      '__REQUIRED_PHP_VERSION_FAILED_1__' => 'Votre version de php ne correspond pas à celle requise par le logiciel. Vous avez besoin de la <strong>Version de PHP</strong> '.htmlentities('=>').' ',
+		),  
+		  );		
+		  
+		?>
