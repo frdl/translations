62e84631e1888696acc33582add036a0a6bf41bb
Till Wehowski <github@webfan.de>
2015-06-22 06:50:41 +0200

    Create main.js

---
+++ frdl/webfan/desktop/main.js
@@ -0,0 +1,71 @@
+{
+
+                     'Modus' : {
+                     	'en_GB'	: 'Mode',
+                        'de_DE' : 'Modus'
+                     },
+                     
+                     
+                     'Language' : {
+                     	'en_GB'	: 'Language',
+                        'de_DE' : 'Sprache'
+                     },
+                      'About' : {
+                     	'en_GB'	: 'About',
+                        'de_DE' : 'Über'
+                     },
+                      'imprint' : {
+                     	'en_GB'	: 'imprint',
+                        'de_DE' : 'Impressum'
+                     },                    
+  
+                        
+                    'title::id:show_messenger' : {
+                        'de_DE' : 'Nachrichtendienst',
+                        'en_GB'	: 'Messenger'
+                     },
+                      
+                     
+   
+                     'No New Notifications' : {
+                     	'en_GB'	: 'No New Notifications',
+                        'de_DE' : 'Keine neuen Benachrichtigungen'
+                     },                  
+                     
+    
+                     'Close' : {
+                     	'en_GB'	: 'Close',
+                        'de_DE' : 'Schliessen'
+                     },                           
+                     
+                     
+                     'Monday' : {
+                       	'en_GB'	: 'Monday',
+                        'de_DE' : 'Montag'
+                     },                   
+                     'Tuesday' : {
+                       	'en_GB'	: 'Tuesday',
+                        'de_DE' : 'Dienstag'
+                     },                   
+                     'Wednesday' : {
+                       	'en_GB'	: 'Wednesday',
+                        'de_DE' : 'Mittwoch'
+                     },                   
+                     'Thursday' : {
+                       	'en_GB'	: 'Thursday',
+                        'de_DE' : 'Donnerstag'
+                     },                   
+                     'Friday' : {
+                       	'en_GB'	: 'Friday',
+                        'de_DE' : 'Freitag'
+                     },                   
+                     'Saturday' : {
+                       	'en_GB'	: 'Saturday',
+                        'de_DE' : 'Samstag'
+                     },                   
+                     'Sunday' : {
+                       	'en_GB'	: 'Sunday',
+                        'de_DE' : 'Sonntag'
+                     }  
+                     
+}
