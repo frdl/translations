848310922fd261fa286ac977217d666c29e77de2
Till Wehowski <github@webfan.de>
2015-07-27 14:09:41 +0200

    /*

---
+++ frdl/webfan/desktop/appication-composer.js
@@ -0,0 +1,79 @@
+
+{
+
+                     'recommended' : {
+                     	'en_GB'	: 'recommended',
+                        'de_DE' : 'empfohlen',
+                        'fr_FR' : 'recommandé'
+                     },
+                     
+                     'All rights reserved' : {
+                     	'en_GB'	: 'All rights reserved',
+                        'de_DE' : 'Alle Rechte vorbehalten',
+                        'fr_FR' : 'Tous droits réservés'
+                     },
+                     
+                     'Password' : {
+                     	'en_GB'	: 'Password',
+                        'de_DE' : 'Passwort',
+                        'fr_FR' : 'Mot de passe'
+                     },
+
+                     'Delete installer after installation' : {
+                     	'en_GB'	: 'Delete installer after installation',
+                        'de_DE' : 'Installer nach der Installation l&ouml;schen',
+                         'fr_FR' : 'Supprimer installateur après l\'installation'
+                     },
+                     
+
+ 
+                     'Update' : {
+                     	'en_GB'	: 'Update',
+                        'de_DE' : 'Aktualisieren',
+                         'fr_FR' : 'Mise à jour'
+                     }      ,
+                     
+ 
+                     'Install to' : {
+                     	'en_GB'	: 'Install to',
+                        'de_DE' : 'Installieren nach',
+                         'fr_FR' : 'Installer pour'
+                     }  ,
+                     
+ 
+                     'Install now' : {
+                     	'en_GB'	: 'Install now',
+                        'de_DE' : 'Jetzt installieren',
+                         'fr_FR' : 'Installer maintenant'
+                     } ,
+                     
+                     
+                     
+                     
+                     
+                     /* todo translates... */
+                     
+                     'Testresult' : {
+                     	'en_GB'	: 'Testresult',
+                        'de_DE' : 'Testergebnis',
+                         'fr_FR' : 'Testresult'
+                     }  ,
+                     
+                     
+                     
+                     'Settings' : {
+                     	'en_GB'	: 'Settings',
+                        'de_DE' : 'Einstellungen',
+                         'fr_FR' : 'Settings'
+                     } ,
+                     
+                     
+                     
+                     'Success' : {
+                     	'en_GB'	: 'Success',
+                        'de_DE' : 'Erfolg',
+                         'fr_FR' : 'Success'
+                     }   
+                     
+                     
+     }
