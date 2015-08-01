a86d891b5a7ab5b3dfe89d6b21412aa18b711c2f
Till Wehowski <github@webfan.de>
2015-08-01 12:20:51 +0200

    Create useraccounts.js

---
+++ frdl/webfan/desktop/useraccounts.js
@@ -0,0 +1,15 @@
+       $($.WebfanDesktop.o.doms.locale).locale('loadLangs', {
+                     'Hello' : {
+                     	'en_GB'	: 'Hello',
+                        'de_DE' : 'Hallo',
+                        'fr_FR' : 'Bonjour'
+                     } ,
+                     
+                      'Create a new user' : {
+                     	'en_GB'	: 'Create a new user',
+                        'de_DE' : 'Neuen Benutzer erstellen',
+                        'fr_FR' : 'Créez un nouvel utilisateur'
+                     }                    
+                     
+                     
+		    }) ; 
