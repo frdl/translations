d67f56df6a9beff2dda56e67e40402eb7425110c
Till Wehowski <github@webfan.de>
2015-08-01 12:23:03 +0200

    Update useraccounts.js

--- frdl/webfan/desktop/useraccounts.js
+++ frdl/webfan/desktop/useraccounts.js
@@ -9,7 +9,13 @@
                      	'en_GB'	: 'Create a new user',
                         'de_DE' : 'Neuen Benutzer erstellen',
                         'fr_FR' : 'Créez un nouvel utilisateur'
-                     }                    
+                     }                     ,
+                     
+                      'Username' : {
+                     	'en_GB'	: 'Username',
+                        'de_DE' : 'Benutzername',
+                        'fr_FR' : 'Nom d\'utilisateur'
+                     }     
                      
                      
 		    }) ; 
