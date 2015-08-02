fdb3b4a6020679385436d55a4d9323f5ac0f2d61
Till Wehowski <github@webfan.de>
2015-08-02 03:01:06 +0200

    Update useraccounts.js

--- frdl/webfan/desktop/useraccounts.js
+++ frdl/webfan/desktop/useraccounts.js
@@ -1,5 +1,6 @@
        $($.WebfanDesktop.o.doms.locale).locale('loadLangs', {
                      
+                     
                       'Create a new user' : {
                      	'en_GB'	: 'Create a new user',
                         'de_DE' : 'Neuen Benutzer erstellen',
@@ -26,6 +27,36 @@
                      	'en_GB'	: 'You can manage different user profiles saved in your browser here.',
                         'de_DE' : 'Sie k&ouml;nnen hier verschiedene in Ihrem Browser gespeicherte Benutzerprofile verwalten.',
                         'fr_FR' : 'You can manage different user profiles saved in your browser here.'
-                     }  
+                     },
+                     
+                      'Your are currently registered as' : {
+                     	'en_GB'	: 'Your are currently registered as',
+                        'de_DE' : 'Sie sind derzeit angemeldet als',
+                        'fr_FR' : 'Your are currently registered as'
+                     },
+                     
+                      'In den personalisierten Browser wechseln?' : {
+                     	'en_GB'	: 'Change to personalized Browser?',
+                        'de_DE' : 'In den personalisierten Browser wechseln?',
+                        'fr_FR' : 'Change to personalized Browser?'
+                     }     ,
+                     
+                      'Personalisierter Browser ist angeschaltet' : {
+                     	'en_GB'	: 'Personalized Browser is enabled',
+                        'de_DE' : 'Personalisierter Browser ist angeschaltet',
+                        'fr_FR' : 'Personalized Browser is enabled'
+                     } ,
+                     
+                      'beenden' : {
+                     	'en_GB'	: 'exit',
+                        'de_DE' : 'beenden',
+                        'fr_FR' : 'exit'
+                     } ,
+                     
+                      'Info lesen...' : {
+                     	'en_GB'	: 'Read info...',
+                        'de_DE' : 'Info lesen...',
+                        'fr_FR' : 'Read info...'
+                     }      
                      
 		    }) ; 
