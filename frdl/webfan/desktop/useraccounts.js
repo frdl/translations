bbed2732018f59be80e8a6af0dd922b9ad7c3c47
Till Wehowski <github@webfan.de>
2015-08-05 23:51:30 +0200

    Update useraccounts.js

--- frdl/webfan/desktop/useraccounts.js
+++ frdl/webfan/desktop/useraccounts.js
@@ -1,4 +1,4 @@
-       $($.WebfanDesktop.o.doms.locale).locale('loadLangs', {
+{
                      
                      
                       'Create a new user' : {
@@ -65,4 +65,4 @@
                         'fr_FR' : 'Delete profile'
                      }          
                      
-		    }) ; 
+}
