88f559b762ca71d9459487b6cf194c5ca9e65882
Till Wehowski <github@webfan.de>
2015-08-24 03:33:46 +0200

    Create postbox.js

---
+++ frdl/webfan/desktop/postbox.js
@@ -0,0 +1,13 @@
+{
+		 	     'Your Private Messages' : {
+                     	'en_GB'	: 'Your Private Messages',
+                        'de_DE' : 'Ihre Privaten Nachrichten',
+                         'fr_FR' : 'Your Private Messages'
+                     } ,
+              
+                 'Write Private Message' : {
+                     	'en_GB'	: 'Write Private Message',
+                        'de_DE' : 'Private Nachricht schreiben',
+                         'fr_FR' : 'Write Private Message'
+                     } 
+}
