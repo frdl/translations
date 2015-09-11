ec29e4ddfe2858e879c364defeb52b3ae3681c33
Till Wehowski <github@webfan.de>
2015-09-12 00:16:47 +0200

    Create package.js

---
+++ frdl/webfan/desktop/package.js
@@ -0,0 +1,10 @@
+{
+
+                     
+                      'No data found' : {
+                     	'en_GB'	: 'No data found',
+                        'de_DE' : 'Keine Daten gefunden',
+                        'fr_FR' : 'No data found'
+                     }   
+                     
+}
