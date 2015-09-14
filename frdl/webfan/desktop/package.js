98f7c80137fdddd76d887ae29ca6246211f8a5d0
Till Wehowski <github@webfan.de>
2015-09-14 02:44:01 +0200

    Update package.js

--- frdl/webfan/desktop/package.js
+++ frdl/webfan/desktop/package.js
@@ -5,6 +5,12 @@
                      	'en_GB'	: 'No data found',
                         'de_DE' : 'Keine Daten gefunden',
                         'fr_FR' : 'No data found'
+                     }   ,
+                    
+                     'Versions' : {
+                     	'en_GB'	: 'Versions',
+                        'de_DE' : 'Versionen',
+                        'fr_FR' : 'Versions'
                      }   
                      
 }
