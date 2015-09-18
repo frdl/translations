f65bff232dfec5e5fc06497978ed74afda472eb7
Till Wehowski <github@webfan.de>
2015-09-18 17:46:08 +0200

    Create themes.js

---
+++ frdl/webfan/desktop/themes.js
@@ -0,0 +1,9 @@
+{
+
+                     
+                      'To install a theme click on' : {
+                     	'en_GB'	: 'To install a theme click on',
+                        'de_DE' : 'Um ein Theme zu installieren klicken Sie auf',
+                        'fr_FR' : 'To install a theme click on'
+                     } 
+}
