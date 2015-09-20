cd26844f04d950dfd272077ad3b8468e984f1f93
Till Wehowski <github@webfan.de>
2015-09-20 19:29:31 +0200

    Update themes.js

--- frdl/webfan/desktop/themes.js
+++ frdl/webfan/desktop/themes.js
@@ -5,5 +5,17 @@
                      	'en_GB'	: 'To install a theme click on',
                         'de_DE' : 'Um ein Theme zu installieren klicken Sie auf',
                         'fr_FR' : 'To install a theme click on'
-                     } 
+                     },
+                     
+                     'Open Theme-File from disk' : {
+                     	'en_GB'	: 'Open Theme-File from disk',
+                        'de_DE' : 'Theme-Datei von Festplatte &ouml;ffnen',
+                        'fr_FR' : 'Open Theme-File from disk'
+                     },
+                     
+                     'To open a theme file click the icon or drag and drop the file into the theme list with your mouse' : {
+                     	'en_GB'	: 'To open a theme file click the icon or drag and drop the file into the theme list with your mouse',
+                        'de_DE' : 'Um eine Theme Datei zu &ouml;ffnen klicken Sie das Symbol oder ziehen Sie Datei mit der Maus in die Theme Liste',
+                        'fr_FR' : 'To open a theme file click the icon or drag and drop the file into the theme list with your mouse'
+                     }
 }
