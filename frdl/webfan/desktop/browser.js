a1ac711b908f1706c9fb1827da56575d9e4eac0f
Till Wehowski <github@webfan.de>
2015-10-14 21:26:50 +0200

    Create browser.js

---
+++ frdl/webfan/desktop/browser.js
@@ -0,0 +1,15 @@
+ {
+
+                     
+                  'Browser' : {
+                     	'en_GB'	: 'Browser',
+                        'de_DE' : 'Browser',
+                        'fr_FR' : 'Browser'
+                     },
+                 
+                     'New window' : {
+                     	'en_GB'	: 'New window',
+                        'de_DE' : 'Neues Fenster',
+                        'fr_FR' : 'New window'
+                     }
+}
