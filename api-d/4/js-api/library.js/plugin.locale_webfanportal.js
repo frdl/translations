cff24e0ffda4e2bc38ed76356959655ca95a15af
Till Wehowski <github@webfan.de>
2015-11-21 20:07:24 +0100

    Create plugin.locale_webfanportal.js

---
+++ api-d/4/js-api/library.js/plugin.locale_webfanportal.js
@@ -0,0 +1,174 @@
+
+(function(){
+	'use strict';
+	
+	var languageStrings = {
+                  
+                  
+                     'Your Account' : {
+                     	'en_GB'	: 'Your Account',
+                        'de_DE' : 'Dein Account',
+                        'fr_FR' : 'Your Account'
+                     } ,             	
+  
+                     
+                      'News' : {
+                     	'en_GB'	: 'News',
+                        'de_DE' : 'Nachrichten',
+                        'fr_FR' : 'News'
+                     }   ,             	
+  
+                     
+                      'Nachrichten' : {
+                     	'en_GB'	: 'News',
+                        'de_DE' : 'Nachrichten',
+                        'fr_FR' : 'News'
+                     }    ,             	
+  
+                     
+                      'Impressum' : {
+                     	'en_GB'	: 'Imprint',
+                        'de_DE' : 'Impressum',
+                        'fr_FR' : 'Imprint'
+                     }  ,             	
+  
+                     
+                      'Kleinanzeigen' : {
+                     	'en_GB'	: 'Class. Ads',
+                        'de_DE' : 'Kleinanzeigen',
+                        'fr_FR' : 'Class. Ads'
+                     } ,             	
+  
+                     
+                      'Lizenzen' : {
+                     	'en_GB'	: 'Licenses',
+                        'de_DE' : 'Lizenzen',
+                        'fr_FR' : 'Licenses'
+                     }  ,             	
+  
+                     
+                      'Einstellungen' : {
+                     	'en_GB'	: 'Settings',
+                        'de_DE' : 'Einstellungen',
+                        'fr_FR' : 'Paramètres'
+                     }  ,             	
+  
+                     
+                      'Suche' : {
+                     	'en_GB'	: 'Search',
+                        'de_DE' : 'Suche',
+                        'fr_FR' : 'Recherche'
+                     }   ,             	
+  
+                     
+                      'Kontakte' : {
+                     	'en_GB'	: 'Contacts',
+                        'de_DE' : 'Kontakte',
+                        'fr_FR' : 'Contacts'
+                     }  ,
+                     
+                      'Kontakt' : {
+                     	'en_GB'	: 'Contact',
+                        'de_DE' : 'Kontakt',
+                        'fr_FR' : 'Contact'
+                     }  ,             	
+  
+                     
+                      'Deine' : {
+                     	'en_GB'	: 'Your',
+                        'de_DE' : 'Deine',
+                        'fr_FR' : 'Your'
+                     }    , 
+                     
+                     'Neue' : {
+                     	'en_GB'	: 'New',
+                        'de_DE' : 'Neue',
+                        'fr_FR' : 'Nouvelle'
+                     }    ,             	
+  
+                     
+                      'Benutzerdaten' : {
+                     	'en_GB'	: 'Userdata',
+                        'de_DE' : 'Benutzerdaten',
+                        'fr_FR' : 'Userdata'
+                     }  ,             	
+  
+                     
+                      'privaten Nachrichten' : {
+                     	'en_GB'	: 'private messages',
+                        'de_DE' : 'privaten Nachrichten',
+                        'fr_FR' : 'private messages'
+                     } ,             	
+                       
+                      'private Nachrichten' : {
+                     	'en_GB'	: 'private messages',
+                        'de_DE' : 'private Nachrichten',
+                        'fr_FR' : 'private messages'
+                     } ,             	
+                       
+                      'Datenschutz' : {
+                     	'en_GB'	: 'pivacy',
+                        'de_DE' : 'Datenschutz',
+                        'fr_FR' : 'pivacy'
+                     } ,              	
+                       
+                      'pivacy policy' : {
+                     	'en_GB'	: 'pivacy policy',
+                        'de_DE' : 'Datenschutzerkl&auml;rung',
+                        'fr_FR' : 'pivacy policy'
+                     }   ,           	
+                       
+                      
+                     
+                     
+                      'Hilfe' : {
+                     	'en_GB'	: 'Help',
+                        'de_DE' : 'Hilfe',
+                        'fr_FR' : 'Aide'
+                     }  ,
+                     
+                     
+                      'Kunden' : {
+                     	'en_GB'	: 'Customers',
+                        'de_DE' : 'Kunden',
+                        'fr_FR' : 'Customers'
+                     } ,
+                     
+                     
+                      'Developers' : {
+                     	'en_GB'	: 'Developers',
+                        'de_DE' : 'Entwickler',
+                        'fr_FR' : 'Developers'
+                     }  ,
+                     
+                     'Entwickler' : {
+                     	'en_GB'	: 'Developers',
+                        'de_DE' : 'Entwickler',
+                        'fr_FR' : 'Developers'
+                     }  ,
+                    
+                    
+                    'Suche' : {
+                     	'en_GB'	: 'Search',
+                        'de_DE' : 'Suche',
+                        'fr_FR' : 'Recherche'
+                     }  ,
+                     
+                     'und' : {
+                     	'en_GB'	: 'and',
+                        'de_DE' : 'und',
+                        'fr_FR' : 'et'
+                     }        
+                                      
+     };
+     
+     
+ 
+   frdl.lang(languageStrings);
+
+	
+}());
+
+
+
+
