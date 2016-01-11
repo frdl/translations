45c0430607b6cb36fd6fd733d28be727651ba69b
Till Wehowski <github@webfan.de>
2016-01-11 13:02:50 +0100

    Create plugin.locale_contacts.js

---
+++ api-d/4/js-api/library.js/plugin.locale_contacts.js
@@ -0,0 +1,50 @@
+frdl.lang({
+                  
+                  
+                     'Kontakt' : {
+                     	'en_GB'	: 'Contact',
+                        'de_DE' : 'Kontakt',
+                        'fr_FR' : 'Contact'
+                     },
+                     
+                    'Kontaktangaben' : {
+                     	'en_GB'	: 'Contactinformation',
+                        'de_DE' : 'Kontaktangaben',
+                        'fr_FR' : 'Donnée contact'
+                     },
+                     
+                    'Notizen' : {
+                     	'en_GB'	: 'Notes',
+                        'de_DE' : 'Notizen',
+                        'fr_FR' : 'Notes'
+                     },
+                     
+                    'zu diesem Kontakt' : {
+                     	'en_GB'	: 'about this contact',
+                        'de_DE' : 'zu diesem Kontakt',
+                        'fr_FR' : 'sur le contact'
+                     },
+                     
+                    'Visitenkarte' : {
+                     	'en_GB'	: 'Business card',
+                        'de_DE' : 'Visitenkarte',
+                        'fr_FR' : 'Carte de visite'
+                     },
+                     
+                    'Kontakte bearbeiten:' : {
+                     	'en_GB'	: 'Edit contacts:',
+                        'de_DE' : 'Kontakte bearbeiten:',
+                        'fr_FR' : 'Modifier les contacts:'
+                     },
+                     
+                    'Kontaktkategorie' : {
+                     	'en_GB'	: 'Contact Category',
+                        'de_DE' : 'Kontaktkategorie',
+                        'fr_FR' : 'Contactez Catégorie'
+                     }
+                     
+                                      
+});
+
+
+
