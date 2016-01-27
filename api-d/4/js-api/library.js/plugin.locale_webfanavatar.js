beef858253e6c381af10f639767d0d587b6529f1
Till Wehowski <github@webfan.de>
2016-01-27 18:36:05 +0100

    Create plugin.locale_webfanavatar.js

---
+++ api-d/4/js-api/library.js/plugin.locale_webfanavatar.js
@@ -0,0 +1,59 @@
+frdl.lang({
+                  
+           'Dein alter Avatar wurde Dir per E-Mail zugesendet, bitte schaue in Dein E-Mail Postfach' : {
+            'en_GB'	: 'Your old avatar was sent to you via e-mail, please look in your e-mail mailbox',
+            'de_DE' : 'Dein alter Avatar wurde Dir per E-Mail zugesendet, bitte schaue in Dein E-Mail Postfach',
+            'fr_FR' : 'Votre ancien avatar vous a été envoyé par e-mail, s\'il vous plaît regardez dans votre boîte aux lettres électronique'
+           },
+           
+           'Dein aktueller Avatar' : {
+            'en_GB'	: 'Your current avatar',
+            'de_DE' : 'Dein aktueller Avatar',
+            'fr_FR' : 'Votre avatar actuel'
+           },
+           
+           'Bilder auf' : {
+            'en_GB'	: 'Images on',
+            'de_DE' : 'Bilder auf',
+            'fr_FR' : 'Images sur'
+           },
+           
+           'Bild hochladen' : {
+            'en_GB'	: 'Upload Image',
+            'de_DE' : 'Bild hochladen',
+            'fr_FR' : 'Envoyer une image'
+           },
+           
+           'erstellen' : {
+            'en_GB'	: 'create',
+            'de_DE' : 'erstellen',
+            'fr_FR' : 'dresser'
+           },
+           
+           'MAKE AVATAR FROM THIS PICTURE' : {
+            'en_GB'	: 'MAKE AVATAR FROM THIS PICTURE',
+            'de_DE' : 'ERSTELLE EIN AVATAR VON DIESEM BILD',
+            'fr_FR' : 'FAIRE AVATAR DE CETTE PHOTO'
+           },
+           
+           'show picture' : {
+            'en_GB'	: 'show picture',
+            'de_DE' : 'Bild anzeigen',
+            'fr_FR' : 'montrer l\'image'
+           },
+           
+           'Erstelle einen' : {
+            'en_GB'	: 'Create one',
+            'de_DE' : 'Erstelle einen',
+            'fr_FR' : 'Créer'
+           },
+           
+           'in 3 einfachen Schritten' : {
+            'en_GB'	: 'in 3 simple steps',
+            'de_DE' : 'in 3 einfachen Schritten',
+            'fr_FR' : 'en 3 étapes simples'
+           }
+           
+           
+           
+});
