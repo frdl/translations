e85e9dede7bccadc45165080645ce945a48058e1
Till Wehowski <github@webfan.de>
2016-07-05 06:49:28 +0200

    Create start.helper.js

---
+++ cdn/application/webfan/locale/module/start.helper.js
@@ -0,0 +1,44 @@
+
+
+exports = module.exports = {
+                  
+                  
+                     'Settings' : {
+                     	'en_GB'	: 'Settings',
+                        'de_DE' : 'Einstellungen',
+                        'fr_FR' : 'Paramètres'
+                     } ,
+                     
+                     
+                     
+                         
+                  
+                     'About...' : {
+                     	'en_GB'	: 'About...',
+                        'de_DE' : 'Über...',
+                        'fr_FR' : 'à propos...'
+                     } ,
+                     
+                     
+                     
+                         
+                  
+                     'Developer' : {
+                     	'en_GB'	: 'Developer',
+                        'de_DE' : 'Entwickler',
+                        'fr_FR' : 'Developer'
+                     } ,
+                     
+                     
+                     
+                         
+                  
+                     'Language' : {
+                     	'en_GB'	: 'Language',
+                        'de_DE' : 'Sprache',
+                        'fr_FR' : 'Language'
+                     } 
+                     
+                     
+                     
+};
