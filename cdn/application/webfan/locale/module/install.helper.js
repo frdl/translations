5062f0c76fa838b4631c14bbde27a5559fddbd54
Till Wehowski <github@webfan.de>
2016-07-05 06:48:33 +0200

    Create install.helper.js

---
+++ cdn/application/webfan/locale/module/install.helper.js
@@ -0,0 +1,45 @@
+/*
+frdl.lang({
+                  
+                  
+                     'Settings' : {
+                     	'en_GB'	: 'Settings',
+                        'de_DE' : 'Einstellungen',
+                        'fr_FR' : 'Paramètres'
+                     } 
+                     
+});
+
+
+*/
+
+exports = module.exports = {
+                  
+                  
+                     'I accept the' : {
+                     	'en_GB'	: 'I accept the',
+                        'de_DE' : 'Ich akzeptiere die',
+                        'fr_FR' : 'J\'accepte le'
+                     } ,
+                     
+                     
+                     
+                         
+                  
+                     'license' : {
+                     	'en_GB'	: 'Licence',
+                        'de_DE' : 'Lizenz',
+                        'fr_FR' : 'Licence'
+                     } ,
+                     
+                     
+                     
+                         
+                  
+                     'Install now' : {
+                     	'en_GB'	: 'Install now',
+                        'de_DE' : 'Jetzt installieren',
+                        'fr_FR' : 'Install now'
+                     } 
+                     
+};
