e0eb23369f9b86c340d0fa7aafd3bb9947b17dcd
Till Wehowski <github@webfan.de>
2016-07-29 22:30:49 +0200

    Create account.helper.js

---
+++ cdn/application/webfan/locale/module/account.helper.js
@@ -0,0 +1,38 @@
+
+exports = module.exports = {
+                  
+                  
+                     'Account' : {
+                     	'en_GB'	: 'Account',
+                        'de_DE' : 'Benutzerkonto',
+                        'fr_FR' : 'Compte'
+                     },
+                     
+                     
+                     
+                      'Passwort vergessen?' : {
+                     	'en_GB'	: 'Forgot Password?',
+                        'de_DE' : 'Passwort vergessen?',
+                        'fr_FR' : 'Mot de passe oublié?'
+                     },
+                     
+                     
+                      'Neuen Benutzer anmelden' : {
+                     	'en_GB'	: 'Register New User',
+                        'de_DE' : 'Neuen Benutzer anmelden',
+                        'fr_FR' : 'Inscription Nouvel utilisateur'
+                     },
+                     
+                     
+                      'Benutzer- Login' : {
+                     	'en_GB'	: 'Sign In',
+                        'de_DE' : 'Benutzer- Login',
+                        'fr_FR' : 'Se connecter'
+                     }
+                     
+                     
+                                          
+                     
+                     
+                     
+};
