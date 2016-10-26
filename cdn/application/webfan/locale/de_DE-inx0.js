5856b29997a917aa978acd8864b01dc190056eff
Till Wehowski <github@webfan.de>
2016-10-26 15:08:01 +0200

    Create de_DE-inx0.js

---
+++ cdn/application/webfan/locale/de_DE-inx0.js
@@ -0,0 +1,9 @@
+frdl.inX.addDict({
+          
+           'developers' : '%s Entwickler',
+           'My.Webfan WebApp for %s.' : 'My.Webfan WebApp für %s.',
+           'you may fill that later' : 'dies können Sie auch später ausfüllen',
+           'Welcome' : 'Herzlich Willkommen %s',
+           'workspace' : 'Arbeitsplatz',
+           'PREVIEW' : 'VORSCHAU'
+}); 		
