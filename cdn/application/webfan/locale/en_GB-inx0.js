e773c08b13fbc8a326b117ef0ebf80552542b0cd
Till Wehowski <github@webfan.de>
2016-10-26 15:07:35 +0200

    Create en_GB-inx0.js

---
+++ cdn/application/webfan/locale/en_GB-inx0.js
@@ -0,0 +1,11 @@
+frdl.inX.addDict({
+          
+           'developers' : '%s  developers',
+           'you may fill that later' : 'you may fill out that later',
+           'My.Webfan WebApp for %s.' : 'My.Webfan WebApppour for %s.',
+           
+           'Welcome' : 'Welcome %s',
+           'more' : 'more',
+           'workspace' : 'workspace',
+           'PREVIEW' : 'PREVIEW'
+}); 	
